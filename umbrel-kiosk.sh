37fc251d2d04df34b174a652133c62a92ea20eed
oconical <oconical@mail.com>
2025-09-16 15:20:19 -0600

    Update umbrel-kiosk.sh
    removed logging into kiosk user before adding autostart configuration

--- umbrel-kiosk.sh
+++ umbrel-kiosk.sh
@@ -6,7 +6,6 @@ sudo apt install -y xorg openbox chromium lightdm
 sudo useradd -m kiosk
 sudo sed -i '/#autologin-user=/c\autologin-user=kiosk' /etc/lightdm/lightdm.conf 
 sudo sed -i '/#user-session=/c\user-session=openbox' /etc/lightdm/lightdm.conf 
-sudo su - kiosk
 
 sudo mkdir -p /home/kiosk/.config/openbox
 sudo printf "chromium --kiosk http://umbrel.local" > /home/kiosk/.config/openbox/autostart
