5e0551757f4695de2242e53d5aa2c0c0b360455d
oconical <oconical@mail.com>
2025-09-16 14:30:20 -0600

    Add files via upload

---
+++ umbrel-update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt install -y xorg openbox chromium lightdm
+sudo useradd -m kiosk
+sudo sed -i '/#autologin-user=/c\autologin-user=kiosk' /etc/lightdm/lightdm.conf 
+sudo sed -i '/#user-session=/c\user-session=openbox' /etc/lightdm/lightdm.conf 
+sudo su - kiosk
+
+sudo mkdir -p /home/kiosk/.config/openbox
+sudo printf "chromium --kiosk http://umbrel.local" > /home/kiosk/.config/openbox/autostart
+
+sudo reboot
